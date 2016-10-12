635c3a4c3f006b6ae666794003a9ee539307636c
Jesus F. Trujillo <jesusft@yahoo-inc.com>
2016-10-11 20:50:38 -0700

    implement SwipeRefreshLayout

--- app/src/main/java/com/ea7jmf/flickster/MovieActivity.java
+++ app/src/main/java/com/ea7jmf/flickster/MovieActivity.java
@@ -1,6 +1,7 @@
 package com.ea7jmf.flickster;
 
 import android.os.Bundle;
+import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.ListView;
 
@@ -19,8 +20,13 @@ import cz.msebera.android.httpclient.Header;
 
 public class MovieActivity extends AppCompatActivity {
 
+    private static String url = "https://api.themoviedb.org/3/movie/now_playing?api_key=a07e22bc18f5cb106bfe4cc1f83ad8ed";
+
+    AsyncHttpClient client;
     ArrayList<Movie> movies;
     MovieArrayAdapter movieAdapter;
+
+    SwipeRefreshLayout swipeContainer;
     ListView lvItems;
 
     @Override
@@ -28,31 +34,51 @@ public class MovieActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_movie);
 
+        swipeContainer = (SwipeRefreshLayout) findViewById(R.id.swipeContainer);
         lvItems = (ListView) findViewById(R.id.lvMovies);
+
         movies = new ArrayList<>();
         movieAdapter = new MovieArrayAdapter(this, movies);
         lvItems.setAdapter(movieAdapter);
 
-        String url = "https://api.themoviedb.org/3/movie/now_playing?api_key=a07e22bc18f5cb106bfe4cc1f83ad8ed";
+        client = new AsyncHttpClient();
+        fetchNowPlaying();
 
-        AsyncHttpClient client = new AsyncHttpClient();
+        swipeContainer.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+                fetchNowPlaying();
+            }
+        });
+
+        swipeContainer.setColorSchemeResources(android.R.color.holo_blue_bright,
+                android.R.color.holo_green_light,
+                android.R.color.holo_orange_light,
+                android.R.color.holo_red_light);
+
+    }
+
+    private void fetchNowPlaying() {
         client.get(url, new JsonHttpResponseHandler() {
             @Override
             public void onSuccess(int statusCode, Header[] headers, JSONObject response) {
-
                 JSONArray moviesJsonResults = null;
-
                 try {
                     moviesJsonResults = response.getJSONArray("results");
+                    movies.clear();
                     movies.addAll(Movie.fromJsonArray(moviesJsonResults));
                     movieAdapter.notifyDataSetChanged();
                 } catch (JSONException e) {
                     e.printStackTrace();
                 }
+
+                swipeContainer.setRefreshing(false);
             }
 
             @Override
             public void onFailure(int statusCode, Header[] headers, String responseString, Throwable throwable) {
+                swipeContainer.setRefreshing(false);
+
                 super.onFailure(statusCode, headers, responseString, throwable);
             }
         });
